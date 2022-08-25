dace2eb5cd3987decb2d0c63f7fc4376444d5a65
Leerena <leerena7@gmail.com>
2022-08-25 19:07:17 +0300

    added eslint, fixed bugs

--- js/lesson9/cart.js
+++ js/lesson9/cart.js
@@ -2,7 +2,8 @@
 
 // Продолжим работу с cart.js
 // Обнаружена уязвимость в нашем функционал
-// Если после добавления последнего товара присвоить к totalPrice любое значение,
+// Если после добавления последнего товара
+// присвоить к totalPrice любое значение,
 // например
 // cart.totalPrice = 10;
 // то при выводе print() общая стоимость корзины будет равна 10
@@ -10,43 +11,44 @@
 //  который будет возвращать результат вызова метода calculateItemPrice
 // метод getTotalPrice больше не нужен
 
-// calculateItemPrice переделать таким образом, чтобы сумму он возвращал, а не записывал в свойство totalPrice
+// calculateItemPrice переделать таким образом, чтобы сумму он возвращал,
+// а не записывал в свойство totalPrice
 
 // Вызов метода calculateItemPrice оставить только у геттера totalPrice
 
 const cart = {
     items: [],
     get totalPrice() {
-      return  cart.calculateItemPrice();  
+        return cart.calculateItemPrice();
     },
     count: 0,
     add(productName = '', productPrice, productQuantity = 1) {
         const product = {
-            productName: productName,
-            productPrice: productPrice,
-            productQuantity: productQuantity,
+            productName,
+            productPrice,
+            productQuantity,
         };
         cart.items.push(product);
         cart.increaseCount(0);
     },
     increaseCount(num) {
         cart.items.reduce((totalCount, currentCount) => {
-            totalCount = totalCount + currentCount.productQuantity;
+            totalCount += currentCount.productQuantity;
             this.count = totalCount;
             return this.count;
-        },0);
+        }, 0);
 
         return this.count += num;
     },
     calculateItemPrice() {
         const sum = cart.items.reduce((total, currentItem) => {
-            total = total + currentItem.productPrice * currentItem.productQuantity;
+            total += currentItem.productPrice * currentItem.productQuantity;
             return total;
-        },0); 
+        }, 0);
         return sum;
     },
     clear() {
-        while(cart.items.length > 0) {
+        while (cart.items.length > 0) {
             cart.items.pop();
             this.totalPrice = 0;
             this.count = 0;
@@ -57,7 +59,7 @@ const cart = {
         console.log(itemsStr);
         console.log(`Общая стоимость товаров: ${cart.totalPrice} руб.`);
     },
-}
+};
 
 cart.add('one', 100, 2);
 cart.add('two', 200, 1);
